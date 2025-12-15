9aff912023b179e09a0db841aebab7cb45f95cf9
Abdulrahim M. <unvacc80@gmail.com>
2025-12-15 08:20:43 +0200

    first commit

---
+++ src/main/java/edu/uofk/ea/association_website_backend/AssociationWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.uofk.ea.association_website_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociationWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssociationWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uofk/ea/association_website_backend/AssociationWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uofk.ea.association_website_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociationWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
