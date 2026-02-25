e504714b577af99ea03f3a58743fe76802f8df4b
Abdulrahim M. <unvacc80@gmail.com>
2026-02-25 14:17:19 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO admins (name, password_hash, verified)
-    SELECT 'admin', '$2a$12$j8RZmQIkCjb5dLz2d9RxXeAwM7151yfzr6zM20opECZUCephkhDUC', 1
-    WHERE NOT EXISTS (SELECT 1 FROM admins); -- The password is 'its-hashed', hashed with BCrypt with strength = 12
+    SELECT 'admin', '$2a$12$jYsFqXS.o6daj//dsz1OQedMdJ/5zQBWxJB3Qk/ICbDUKGtAVE7wi', 1
+    WHERE NOT EXISTS (SELECT 1 FROM admins); -- The password is 'itshashed', hashed with BCrypt with strength = 12
 INSERT INTO admin_roles (admin_id, role)
     SELECT 1, 'ROLE_SUPER_ADMIN'
     WHERE NOT EXISTS (SELECT 1 FROM admin_roles);
